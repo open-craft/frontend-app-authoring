eb30b0624fa19fe9583ae285443c8d23cbfcde9d
Navin Karkera <navin@disroot.org>
2023-12-11 19:48:18 +0530

    refactor: fix jsdoc types

--- src/course-outline/utils.jsx
+++ src/course-outline/utils.jsx
@@ -13,7 +13,7 @@ import { SECTION_BADGE_STATUTES, STAFF_ONLY } from './constants';
  * @param {bool} visibleToStaffOnly - value from section info
  * @param {string} visibilityState - value from section info
  * @param {bool} staffOnlyMessage - value from section info
- * @returns {typeof SECTION_BADGE_STATUTES}
+ * @returns {SECTION_BADGE_STATUTES[keyof SECTION_BADGE_STATUTES]}
  */
 const getSectionStatus = ({
   published,
