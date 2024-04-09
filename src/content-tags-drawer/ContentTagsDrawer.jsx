ad15c0436fb674dafd3523bf9a1bd34c77da6551
Yusuf Musleh <yusuf@opencraft.com>
2024-04-09 15:54:55 +0300

    feat: Move dropdown arrows to left of taxonomy

--- src/content-tags-drawer/ContentTagsDrawer.jsx
+++ src/content-tags-drawer/ContentTagsDrawer.jsx
@@ -97,7 +97,7 @@ const ContentTagsDrawer = ({ id, onClose }) => {
           )}
 
         <hr />
-        <p className='lead text-gray-500 font-weight-bold'>{intl.formatMessage(messages.headerSubtitle)}</p>
+        <p className="lead text-gray-500 font-weight-bold">{intl.formatMessage(messages.headerSubtitle)}</p>
 
         { isTaxonomyListLoaded && isContentTaxonomyTagsLoaded
           ? tagsByTaxonomy.map((data) => (
