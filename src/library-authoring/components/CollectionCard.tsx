b6ff6c8a1b8965f65fcc09c60a9059d365b9933d
Braden MacDonald <braden@opencraft.com>
2024-09-25 13:33:57 -0700

    feat: temporarily add the "Open" button from Romulo's collections PR

--- src/library-authoring/components/CollectionCard.tsx
+++ src/library-authoring/components/CollectionCard.tsx
@@ -1,20 +1,48 @@
-import { useIntl } from '@edx/frontend-platform/i18n';
+import { useIntl, FormattedMessage } from '@edx/frontend-platform/i18n';
 import {
   ActionRow,
+  Dropdown,
   Icon,
   IconButton,
 } from '@openedx/paragon';
 import { MoreVert } from '@openedx/paragon/icons';
+import { Link } from 'react-router-dom';
 
 import { type CollectionHit } from '../../search-manager';
-import messages from './messages';
 import BaseComponentCard from './BaseComponentCard';
+import messages from './messages';
+
+export const CollectionMenu = ({ collectionHit }: { collectionHit: CollectionHit }) => {
+  const intl = useIntl();
+
+  return (
+    <Dropdown id="collection-card-dropdown" onClick={(e) => e.stopPropagation()}>
+      <Dropdown.Toggle
+        id="collection-card-menu-toggle"
+        as={IconButton}
+        src={MoreVert}
+        iconAs={Icon}
+        variant="primary"
+        alt={intl.formatMessage(messages.collectionCardMenuAlt)}
+        data-testid="collection-card-menu-toggle"
+      />
+      <Dropdown.Menu>
+        <Dropdown.Item
+          as={Link}
+          to={`/library/${collectionHit.contextKey}/collection/${collectionHit.blockId}/`}
+        >
+          <FormattedMessage {...messages.menuOpen} />
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
 
 type CollectionCardProps = {
   collectionHit: CollectionHit,
 };
 
-const CollectionCard = ({ collectionHit } : CollectionCardProps) => {
+const CollectionCard = ({ collectionHit }: CollectionCardProps) => {
   const intl = useIntl();
 
   const {
@@ -37,12 +65,7 @@ const CollectionCard = ({ collectionHit } : CollectionCardProps) => {
       tags={tags}
       actions={(
         <ActionRow>
-          <IconButton
-            src={MoreVert}
-            iconAs={Icon}
-            variant="primary"
-            alt={intl.formatMessage(messages.collectionCardMenuAlt)}
-          />
+          <CollectionMenu collectionHit={collectionHit} />
         </ActionRow>
       )}
       blockTypeDisplayName={blockTypeDisplayName}
--- src/search-manager/data/api.ts
+++ src/search-manager/data/api.ts
@@ -101,6 +101,8 @@ interface BaseContentHit {
   id: string;
   type: 'course_block' | 'library_block' | 'collection';
   displayName: string;
+  usageKey: string;
+  blockId: string;
   /** The course or library ID */
   contextKey: string;
   org: string;
@@ -117,8 +119,6 @@ interface BaseContentHit {
  * Defined in edx-platform/openedx/core/djangoapps/content/search/documents.py
  */
 export interface ContentHit extends BaseContentHit {
-  usageKey: string;
-  blockId: string;
   /** The block_type part of the usage key. What type of XBlock this is. */
   blockType: string;
   /**
