9897cdedd0b8e97e5c8e4918c3e0ad34253c3ac4
Jillian Vogel <jill@opencraft.com>
2024-10-17 17:44:28 +1030

    test: improve test coverage

--- src/studio-home/tabs-section/TabsSection.test.tsx
+++ src/studio-home/tabs-section/TabsSection.test.tsx
@@ -15,6 +15,7 @@ import {
 import { getApiBaseUrl, getStudioHomeApiUrl } from '../data/api';
 import { executeThunk } from '../../utils';
 import { fetchLibraryData, fetchStudioHomeData } from '../data/thunks';
+import { mockGetContentLibraryV2List } from '../../library-authoring/data/api.mocks';
 import { getContentLibraryV2ListApiUrl } from '../../library-authoring/data/api';
 import contentLibrariesListV2 from '../../library-authoring/__mocks__/contentLibrariesListV2';
 import {
@@ -391,6 +392,7 @@ describe('<TabsSection />', () => {
       expect(librariesTab).toHaveClass('active');
 
       expect(screen.getByText('Showing 2 of 2')).toBeVisible();
+      expect(screen.getByText('Page 1, Current Page, of 2')).toBeVisible();
 
       expect(screen.getByText(contentLibrariesListV2.results[0].title)).toBeVisible();
       expect(screen.getByText(
@@ -400,6 +402,22 @@ describe('<TabsSection />', () => {
       expect(screen.getByText(contentLibrariesListV2.results[1].title)).toBeVisible();
       expect(screen.getByText(
         `${contentLibrariesListV2.results[1].org} / ${contentLibrariesListV2.results[1].slug}`,
+      )).toBeVisible();
+    });
+
+    it('should show a "not found" message if no v2 libraries were loaded', async () => {
+      mockGetContentLibraryV2List.applyMockEmpty();
+      render();
+      axiosMock.onGet(getStudioHomeApiUrl()).reply(200, generateGetStudioHomeDataApiResponse());
+      await executeThunk(fetchStudioHomeData(), store.dispatch);
+
+      const librariesTab = screen.getByRole('tab', { name: librariesBetaTabTitle });
+      fireEvent.click(librariesTab);
+
+      expect(librariesTab).toHaveClass('active');
+
+      expect(await screen.findByText(
+        tabMessages.librariesV2TabLibraryNotFoundAlertMessage.defaultMessage,
       )).toBeVisible();
     });
 
@@ -414,7 +432,7 @@ describe('<TabsSection />', () => {
       expect(screen.queryByText(tabMessages.legacyLibrariesTabTitle.defaultMessage)).toBeNull();
     });
 
-    it('should render libraries fetch failure alert', async () => {
+    it('should render legacy libraries fetch failure alert', async () => {
       render();
       axiosMock.onGet(getStudioHomeApiUrl()).reply(200, generateGetStudioHomeDataApiResponse());
       axiosMock.onGet(libraryApiLink).reply(404);
