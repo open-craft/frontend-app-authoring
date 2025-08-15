469210bb098dc399b9dafd1e89ba8932ec9c8330
Kshitij Sobti <kshitij@sobti.in>
2025-08-15 07:42:07 +0530

    fix: Replace `Truncate.Deprecated` with CSS-based truncation (#2374)
    The Truncate element as it exists has a bug where it can end up in an infinite loop when truncating to a very small size on mobiles. This makes the course outline view unresponsive on mobile and can lead to a crash.
    
    This replaces the Truncate element with some CSS.
    
    (cherry picked from commit 53e925e07a23ed25de6ff7a68c5160bf7d42f8c1)
    (cherry picked from commit ece5d470d94ed1aaa6b4ba7150323205fe699d7a)

--- src/course-outline/card-header/TitleButton.jsx
+++ src/course-outline/card-header/TitleButton.jsx
@@ -4,7 +4,6 @@ import {
   Button,
   OverlayTrigger,
   Tooltip,
-  Truncate,
 } from '@openedx/paragon';
 import {
   ArrowDropDown as ArrowDownIcon,
@@ -39,7 +38,9 @@ const TitleButton = ({
         className="item-card-header__title-btn"
         onClick={onTitleClick}
       >
-        <Truncate.Deprecated lines={1} className={`${namePrefix}-card-title mb-0`}>{title}</Truncate.Deprecated>
+        <span className="truncate-1-line">
+          {title}
+        </span>
       </Button>
     </OverlayTrigger>
   );
